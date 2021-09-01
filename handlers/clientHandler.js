f3cb5af9519f60947f11ef42c046b688825fd753
Bhuvnesh <bhuvankumar9944@gmail.com>
2021-09-01 12:24:45 +0530

    Fine

--- handlers/clientHandler.js
+++ handlers/clientHandler.js
@@ -4,9 +4,4 @@ module.exports = (client) => {
   client.commands = new Collection();
   client.slashCommands = new Collection();
   client.events = new Collection();
-
-  // Functions
-  String.prototype.capitalize = function() {
-    return this[0].toUpperCase() + this.slice(1)
-  }
 };
