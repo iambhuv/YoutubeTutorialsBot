9bb12c884ce12bf5b204dec39d00973ac19332d9
Bhuvnesh <bhuvankumar9944@gmail.com>
2021-09-02 09:14:29 +0530

    Work Done

--- handlers/slashCommandHandler.js
+++ handlers/slashCommandHandler.js
@@ -4,26 +4,27 @@ module.exports = (client) => {
   const cmdFolders = fs.readdirSync("./SlashCommands");
 
   const cmdArr = [];
-  cmdFolders.forEach(cmdFolder => {
-    const cmdFiles = fs.readdirSync(`./SlashCommands/${cmdFolder}`).filter(f => f.endsWith(".js"));
+  cmdFolders.forEach((cmdFolder) => {
+    const cmdFiles = fs.readdirSync(`./SlashCommands/${cmdFolder}`).filter((f) => f.endsWith(".js"));
 
-    cmdFiles.forEach(file => {
-      const command = require(`../SlashCommands/${cmdFolder}/${file}`)
+    cmdFiles.forEach((file) => {
+      const command = require(`../SlashCommands/${cmdFolder}/${file}`);
 
       if (command.name && command.execute) {
+        command.directory = cmdFolder.split("_")[cmdFolder.split("_").length - 1];
         client.slashCommands.set(command.name, command);
-        cmdArr.push(command)
+        cmdArr.push(command);
       }
-    })
+    });
   });
 
-  client.on('ready', async () => {
+  client.on("ready", async () => {
     try {
-      cmdArr.forEach(async cmd => {
-        await client.guilds.cache.forEach(async guild =>{
-          await guild?.commands.create(cmd).catch(err => {})
-        })
-      })
+      cmdArr.forEach(async (cmd) => {
+        await client.guilds.cache.forEach(async (guild) => {
+          await guild?.commands.create(cmd).catch((err) => {});
+        });
+      });
     } catch (err) {}
-  })
-}
+  });
+};
